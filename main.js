ab447f7b7080c7d92083cfd408ac835d87d4f725
Mr. Joe <radics.pepe@gmail.com>
2022-11-16 21:13:56 +0100

    save_11.16

---
+++ main.js
@@ -0,0 +1,39 @@
+const products = [
+  {
+    name: "Áfonya",
+    picture: "afonya.jpg",
+    description: "Kézzel termelt egészség",
+    price: 3000,
+    inStock: true,
+  },
+
+  {
+    name: "Szölő",
+    picture: "feher-szolo.webp",
+    description: "Kézzel termelt egészség",
+    price: 2000,
+    inStock: true,
+    variations: ["fehér", "kék"],
+  },
+
+  {
+    name: "Paradicsom",
+    picture: "paradicsom.webp",
+    description: "Kézzel termelt egészség",
+    price: 1000,
+    inStock: true,
+  },
+];
+
+const productSection = document.getElementById("product");
+
+//TODO Ez egy proba feladat
+
+productSection.innerHTML = `<div>
+<h2>${products[0].name}</h2>
+<p>${products[0].description}</p>
+<img src="./img/${products[0].picture}">
+<h3>${products[0].price} Ft</h3>
+
+
+</div>`;
