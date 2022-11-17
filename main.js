52afcfde6c663b8d0ec433871eefb5c9cc909b20
unknown <radics.pepe@gmail.com>
2022-11-17 09:10:20 +0100

    I_save_11.17

--- main.js
+++ main.js
@@ -27,13 +27,12 @@ const products = [
 
 const productSection = document.getElementById("product");
 
-//TODO Ez egy proba feladat
+
 
 productSection.innerHTML = `<div>
 <h2>${products[0].name}</h2>
 <p>${products[0].description}</p>
 <img src="./img/${products[0].picture}">
 <h3>${products[0].price} Ft</h3>
-
-
+<a href="#">Kosárba</a>
 </div>`;
